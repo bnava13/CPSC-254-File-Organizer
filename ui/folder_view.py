481808182c2631a926c581861a1be8230ff347c5
Star-dust87 <skim28@csu.fullerton.edu>
2024-12-09 20:17:22 -0800

    Update folder_view.py

--- ui/folder_view.py
+++ ui/folder_view.py
@@ -5,17 +5,15 @@ class FolderView(QTreeView):
     def __init__(self, folder_selected_callback):
         super().__init__()
         self.model = QFileSystemModel()
-        self.model.setRootPath("")
+        self.model.setRootPath(QDir.homePath())
+        self.model.setFilter(QDir.NoDotAndDotDot | QDir.AllDirs)
         self.setModel(self.model)
-        self.setRootIndex(self.model.index(""))
+        self.setRootIndex(self.model.index(QDir.homePath()))
         self.setHeaderHidden(True)
 
          # Hide all columns except the name column
         for column in range(1, self.model.columnCount()):
             self.hideColumn(column)
-            
-        # Only show directories - using QDir.Filter instead of Qt
-        self.model.setFilter(QDir.AllDirs | QDir.NoDotAndDotDot)
 
         # Folder selection callback
         self.clicked.connect(lambda index: folder_selected_callback(self.model.filePath(index)))
